bae6b0d18f17d683041b5e9088fdf78419e8e50b
Justin Evans <justin.robert.evans@protonmail.com>
2024-09-18 13:15:16 +0000

    add updated_on field to Post model,  run migrations JRE 18-09-2024 1415

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    excerpt = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+    excerpt = models.TextField(blank=True)
 # dog fields added below
     dog_name = models.CharField(max_length=200, unique=True)
     dog_type = models.CharField(max_length=200, unique=True)
