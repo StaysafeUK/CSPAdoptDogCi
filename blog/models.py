130d84070c538513043138f02f014e4c1f6c8147
Justin Evans <justin.robert.evans@protonmail.com>
2024-09-18 12:39:01 +0000

    add Post Model,  run mogrations JRE 18-09-2024 1338

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+# dog fields added below
+    dog_name = models.CharField(max_length=200, unique=True)
+    dog_type = models.CharField(max_length=200, unique=True)
+    dog_age = models.IntegerField()
+    dog_stay = models.IntegerField()
+    
+    STATUS = ((0, "Draft"), (1, "Published"))
+    status = models.IntegerField(choices=STATUS, default=0)
+ 
+
+
+
