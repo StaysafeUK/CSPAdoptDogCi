b360a89df6bf657293ab31ea2eb57deb948017ed
Justin Evans <justin.robert.evans@protonmail.com>
2024-09-18 15:40:31 +0000

    add comment and sanctuary model apply migrations JRE 18-09-2024 1640

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 # Create your models here.
 
@@ -19,6 +21,22 @@ class Post(models.Model):
     
     STATUS = ((0, "Draft"), (1, "Published"))
     status = models.IntegerField(choices=STATUS, default=0)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+class Sanctuary(models.Model):
+    dog_sanctuary = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="dog_sanctuaries")
+    sanct_address = models.TextField()
+    sanct_postcode = models.CharField(max_length=15, unique=True)
+    sanct_telephone = PhoneNumberField(blank=True)
+    sanct_email = models.EmailField()
+    sanct_website = models.URLField(max_length=200)
  
 
 
