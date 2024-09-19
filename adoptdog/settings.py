f7903478f961dff669fbec05f35720247a0dd652
Justin Evans <justin.robert.evans@protonmail.com>
2024-09-19 12:03:48 +0000

    install summernote & add to urls.py and settings.py JRE 19-09-2024 1303

--- adoptdog/settings.py
+++ adoptdog/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'phonenumber_field',
     'hello_world',
     'blog',
