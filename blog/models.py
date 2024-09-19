0de419c3c19243689aafc9d247e5ea32752640b3
Justin Evans <justin.robert.evans@protonmail.com>
2024-09-19 11:01:20 +0000

    modified model Sanctuary add dunders JRE 10-09-2024 1201

--- blog/models.py
+++ blog/models.py
@@ -18,9 +18,17 @@ class Post(models.Model):
     dog_type = models.CharField(max_length=200, unique=True)
     dog_age = models.IntegerField()
     dog_stay = models.IntegerField()
+    sanctuary = models.ForeignKey('Sanctuary', on_delete=models.CASCADE, related_name="dogs")
     
     STATUS = ((0, "Draft"), (1, "Published"))
     status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return f" {self.title} | created by {self.author}"
+
+    class Meta:
+        ordering = ["-created_on"]
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
@@ -29,15 +37,24 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+
+    class Meta:
+        ordering = ["created_on"]
+
+    
 
 class Sanctuary(models.Model):
-    dog_sanctuary = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="dog_sanctuaries")
+    sanct_name = models.CharField(max_length=200)
     sanct_address = models.TextField()
     sanct_postcode = models.CharField(max_length=15, unique=True)
     sanct_telephone = PhoneNumberField(blank=True)
     sanct_email = models.EmailField()
     sanct_website = models.URLField(max_length=200)
+
+    def __str__(self):
+        return f"name of sanctuary: {self.sanct_name}"
+
  
-
-
 
