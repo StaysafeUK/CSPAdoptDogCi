39e398dd7ebec9bc6342f55532acc11cac3633c6
Justin Evans <justin.robert.evans@protonmail.com>
2024-09-24 13:29:56 +0000

    prototype confirmation JRE 24-09-2024 1429

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 
 #URL pattern for default site 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+
+    path('signup/', views.signup, name='signup'),
+    path('activate/<slug:uidb64>/<slug:token>/', views.activate_account, name='activate'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
     path('search_sanctuaries', views.search_sanctuaries, name='search-sanctuaries'),
-     path('dog/<slug:slug>/', views.dog_detail, name='dog_detail'),
-
+    path('dog/<slug:slug>/', views.dog_detail, name='dog_detail'),
+     path('', views.PostList.as_view(), name='home')
 ]
