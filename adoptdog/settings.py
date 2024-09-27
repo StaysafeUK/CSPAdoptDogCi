1e4af4a9cc8377f6f848f2d4c23a275fe737bf3c
Justin Evans <justin.robert.evans@protonmail.com>
2024-09-27 14:37:16 +0000

    configuration changed JRE 27-09-2024 1538

--- adoptdog/settings.py
+++ adoptdog/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # EMAIL_USE_TLS = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-staysafeuk-cspadoptdogc-mhajk4u2qj5.ws.codeinstitute-ide.net',
 'https://8000-staysafeuk-cspadoptdogc-mhajk4u2qj5.ws.codeinstitute-ide.net',
