242e7ba57f76edf0a4dda71c719d466b873a6c66
Justin Evans <justin.robert.evans@protonmail.com>
2024-10-04 08:53:30 +0000

    remove sendgrid code from blog App JRE 04-10-2024 0953

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 
 #URL pattern for default site 
 urlpatterns = [
-    # Two comments below for Email authorisation and authentication currently not used 
-    # path('signup/', views.signup, name='signup'),
-    # path('activate/<slug:uidb64>/<slug:token>/', views.activate_account, name='activate'),
+   
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
